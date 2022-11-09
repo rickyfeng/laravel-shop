b824138f9a6e8424d65a66020435f0bcce77113e
Ricky Feng <ricky.feng@163.com>
2022-11-09 22:25:41 +0800

    新增收货地址

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -9,7 +9,7 @@ class UserAddress extends Model
 {
     use HasFactory;
 
-    protected $filllable = [
+    protected $fillable  = [
         'province',
         'city',
         'district',
@@ -17,7 +17,7 @@ class UserAddress extends Model
         'zip',
         'contact_name',
         'contact_phone',
-        'last_used_at'
+        'last_used_at',
     ];
 
     protected $dates = ['last_used_at'];

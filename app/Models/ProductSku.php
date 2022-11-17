5cd8352d90739e2cc9f9a7fa7b75480b89775144
Ricky Feng <ricky.feng@163.com>
2022-11-17 17:50:31 +0800

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\InternalException;
+
 
 class ProductSku extends Model
 {
@@ -15,4 +17,25 @@ class ProductSku extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    // 减库存
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可以小于0');
+        }
+        // 减库存不能简单地通过 update(['stock' => $sku->stock - $amount]) 来操作，
+        // 在高并发的情况下会有问题，这就需要通过数据库的方式来解决。
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount) 
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可以小于0');
+        }
+        $this->increment('stock', $amount);
+    }
+
 }
+
--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,12 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function dda($model)
+{
+    if (method_exists($model, 'toArray')) {
+        dd($model->toArray());
+    } else {
+        dd($model);
+    }
+}

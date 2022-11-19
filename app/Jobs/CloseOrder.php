9de578e0fe6a317504de0cc0d908a391c599daa9
Ricky Feng <ricky.feng@163.com>
2022-11-19 20:42:43 +0800

    关闭未支付订单

---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
+
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order, $delay)
+    {
+        $this->order = $order;
+        // 设置延迟的时间，delay() 方法的参数代表多少秒之后执行
+        $this->delay($delay);        
+    }
+
+    // 定义这个任务类具体的执行逻辑
+    // 当队列处理器从队列中取出任务时，会调用 handle() 方法
+    public function handle()
+    {
+        // 判断对应的订单是否已经被支付
+        // 如果已经支付则不需要关闭订单，直接退出
+        if ($this->order->paid_at) {
+            return;
+        }
+
+        \DB::transaction(function(){
+            $this->order->update(['closed' => true]);
+            foreach($this->order->items as $item) {
+                $item->productSku->addStock($item->amount);
+            }
+        });
+
+    }
+}
